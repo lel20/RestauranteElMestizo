b1d9dc5aa270071df284f059d37b84767d38d6f7
lel20 <leonardoparedes.v.r@gmail.com>
2025-05-30 18:59:35 +0200

    Creación del footer

--- Frontend/mestizoFront/src/components/Navigation.tsx
+++ Frontend/mestizoFront/src/components/Navigation.tsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router";
 import { CiMenuBurger } from "react-icons/ci";
 import { useEffect, useState } from "react";
+import Logo from "../assets/pizza.png";
 export const Navigation = () => {
   //Para controlar el menu despelgable en pantallas móviles
   const [mostrar, setMostrar] = useState(false);
@@ -22,84 +23,83 @@ export const Navigation = () => {
 
   return (
     <>
-      <nav
-        className="fixed h-20 px-12 items-center justify-center flex w-full container mx-auto
-         bg-[rgba(0,0,0,0.5)] space-x-4
-         text-black text-xl"
-      >
-        <div className="flex h-full items-center space-x-4">
-          <img
-            className="w-12 h-12 bg-amber-500 rounded-full"
-            src=""
-            alt="logo"
-          />
-          <p>Mestizo</p>
-        </div>
-        <div className=" h-full flex space-x-4 justify-center items-center">
-          <button>
-            <CiMenuBurger
-              onClick={onMostrar}
-              size={30}
-              className="md:hidden outline "
-            />
-          </button>
-          <div className=" hidden md:flex  h-full ">
-            <ul className="flex justify-center items-center w-full h-full space-x-4">
-              <li className=" h-full  flex items-center justify-center">
-                <NavLink
-                  className={({ isActive }) =>
-                    `transition-all duration-500 h-full flex items-center ${
-                      isActive ? "text-orange-500 scale-x-110" : "text-white"
-                    }`
-                  }
-                  to="/"
-                >
-                  Inicio
-                </NavLink>
-              </li>
-              <li className="h-full flex items-center justify-center">
-                <NavLink
-                  className={({ isActive }) =>
-                    `transition-all duration-500 ${
-                      isActive ? "text-orange-500 scale-x-110" : "text-white"
-                    }`
-                  }
-                  to="/menu"
-                >
-                  Menu
-                </NavLink>
-              </li>
-              <li className="h-full flex items-center justify-center">
-                <NavLink
-                  className={({ isActive }) =>
-                    `transition-all duration-500 ${
-                      isActive ? "text-orange-500 scale-x-110" : "text-white"
-                    }`
-                  }
-                  to="/carta"
-                >
-                  Carta
-                </NavLink>
-              </li>
-              <li className="h-full flex items-center justify-center">
-                <NavLink
-                  className={({ isActive }) =>
-                    `transition-all duration-500  ${
-                      isActive ? "text-orange-500 scale-x-110" : "text-white"
-                    }`
-                  }
-                  to="/contacto"
-                >
-                  Contacto
-                </NavLink>
-              </li>
-            </ul>
+      <nav className="fixed top-0 left-0 w-full h-20 bg-[rgba(0,0,0,0.9)] z-50 text-white text-xl">
+        <div className="container mx-auto flex items-center justify-center h-full space-x-4 px-6">
+          {/* logo y nombre */}
+          <div className="flex items-center space-x-4">
+            {Logo && (
+              <img className="w-14 h-14  rounded-full" src={Logo} alt="" />
+            )}
+
+            <h2>Mestizo</h2>
+          </div>
+
+          <div className="flex items-center space-x-4">
+            <button>
+              <CiMenuBurger
+                onClick={onMostrar}
+                size={30}
+                className="md:hidden "
+              />
+            </button>
+            <div className=" hidden md:flex  h-full ">
+              <ul className="flex justify-center items-center w-full h-full space-x-4">
+                <li className=" h-full  flex items-center justify-center">
+                  <NavLink
+                    className={({ isActive }) =>
+                      `transition-all duration-500 h-full flex items-center ${
+                        isActive ? "text-orange-500 scale-x-110" : "text-white"
+                      }`
+                    }
+                    to="/"
+                  >
+                    Inicio
+                  </NavLink>
+                </li>
+                <li className="h-full flex items-center justify-center">
+                  <NavLink
+                    className={({ isActive }) =>
+                      `transition-all duration-500 ${
+                        isActive ? "text-orange-500 scale-x-110" : "text-white"
+                      }`
+                    }
+                    to="/menu"
+                  >
+                    Menu
+                  </NavLink>
+                </li>
+                <li className="h-full flex items-center justify-center">
+                  <NavLink
+                    className={({ isActive }) =>
+                      `transition-all duration-500 ${
+                        isActive ? "text-orange-500 scale-x-110" : "text-white"
+                      }`
+                    }
+                    to="/carta"
+                  >
+                    Carta
+                  </NavLink>
+                </li>
+                <li className="h-full flex items-center justify-center">
+                  <NavLink
+                    className={({ isActive }) =>
+                      `transition-all duration-500  ${
+                        isActive ? "text-orange-500 scale-x-110" : "text-white"
+                      }`
+                    }
+                    to="/contacto"
+                  >
+                    Contacto
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
       {mostrar && (
-        <div className="fixed  top-15 w-full h-full bg-black/90 text-xl text-white">
-          <ul className="flex flex-col h-full items-center justify-center pace-x-4">
+        <div className="fixed  top-20 left-0 w-full h-full bg-black/90 text-xl text-white">
+          <ul className="flex flex-col h-[90%] items-center justify-center pace-x-4">
             <li className="w-full flex justify-center items-center h-10">
               <NavLink
                 onClick={onMostrar}
--- Frontend/mestizoFront/src/components/Piepagina.tsx
+++ Frontend/mestizoFront/src/components/Piepagina.tsx
@@ -1,16 +1,13 @@
 import { FaSquareInstagram } from "react-icons/fa6";
 import { FaFacebook } from "react-icons/fa";
-
+import Logo from "../assets/pizza.png";
 export const Piepagina = () => {
   return (
-    <footer
-      className=" container mx-auto min-h-80 bg-black/90 text-amber-300 bottom-0 flex-col md:flex-row p-8 justify-center 
-      items-center"
-    >
-      <div className="flex flex-col md:flex-row bg-red-600 w-full justify-center space-x-10 space-y-5">
-        <div className="flex space-x-4">
-          <div>
-            <p>Lunes</p>
+    <footer className="w-full h-[70rem] md:h-[40rem] flex flex-col justify-center  bg-black/90 text-white p-8 space-y-20">
+      <div className="  grid grid-cols-1 md:grid-cols-custom gap-20">
+        <div className="flex-1 flex min-w-[200px] h-full justify-center gap-x-4  items-center w-full ">
+          <div className=" ">
+            <p className="">Lunes</p>
             <p>Martes</p>
             <p>Miércoles</p>
             <p>Jueves</p>
@@ -28,7 +25,18 @@ export const Piepagina = () => {
             <p>10:00h -22:00h </p>
           </div>
         </div>
-        <div className="flex space-x-4">
+        <div className="flex flex-1 flex-col min-w-[200px] justify-center   w-full items-center">
+          {Logo && (
+            <img
+              className="w-40 h-40 bg-amber-50 rounded-full"
+              src={Logo}
+              alt="logo"
+            />
+          )}
+
+          <h1>El Mestizo</h1>
+        </div>
+        <div className="flex flex-1 space-x-4 min-w-[200px] justify-center w-full items-center">
           <a href="">
             <FaSquareInstagram />
           </a>
@@ -36,8 +44,21 @@ export const Piepagina = () => {
             <FaFacebook />
           </a>
         </div>
+        <div className="flex flex-1 flex-col space-x-4 min-w-[200px]  justify-center w-full items-center">
+          <a href="">Inicio</a>
+          <a href="">Menu</a>
+          <a href="">Carta</a>
+          <a href="">Contacto</a>
+          <a href="">Sobre Nosotros</a>
+          <a href="">Galeria</a>
+          <a href="">Filosofía</a>
+        </div>
       </div>
-      <p>© 2025 Todos los derecho reservados a Lenardo Paredes.</p>
+      <div className="container mx-auto">
+        <p className="text-center">
+          © 2025 Todos los derecho reservados a Lenardo Paredes.
+        </p>
+      </div>
     </footer>
   );
 };
